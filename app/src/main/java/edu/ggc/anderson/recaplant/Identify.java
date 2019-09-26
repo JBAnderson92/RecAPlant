b4556e37461766f198c9655a862f42bd049e83a7
JBAnderson92 <JBAnderson92@gmail.com>
2019-09-25 20:48:42 -0400

    Added some UI to the Identify page, Added Camera grab to the Identify page

--- app/src/main/java/edu/ggc/anderson/recaplant/Identify.java
+++ app/src/main/java/edu/ggc/anderson/recaplant/Identify.java
@@ -1,14 +1,42 @@
 package edu.ggc.anderson.recaplant;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Identify extends AppCompatActivity {
+
+    ImageView imageView;
+    public static final int REQUEST_CODE = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identify);
+
+        Button btnCamera = findViewById(R.id.btnCamera);
+        ImageView imageView = findViewById(R.id.ivIdentify);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 }
